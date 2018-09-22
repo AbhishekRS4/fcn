5bd2171a61d37941119b29ddd657ef78dc1057a7
AbhishekRS4 <abhi.apple4@gmail.com>
2018-09-22 12:47:38 +0530

    added color maps from the groundtruth masks

---
+++ color_maps.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+
+color_maps = [(128,  64, 128), # road
+              (244,  35, 232), # sidewalk
+              (250, 170,  30), # traffic light
+              (220, 220,   0), # traffic sign
+              (220,  20,  60), # person
+              (255,   0,   0), # rider
+              (  0,   0, 142), # car
+              (  0,   0,  70), # truck
+              (  0,  60, 100), # bus
+              (  0,   0,  90), # caravan
+              (  0,   0, 110), # trailer
+              (  0,  80, 100), # train
+              (  0,   0, 230), # motorcycle
+              (119,  11,  32), # bicycle
+              (  0,   0,   0)] # other
